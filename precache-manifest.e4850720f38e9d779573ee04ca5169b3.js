708bdb5d0fefe1d0cb802bc7ad5d485a85529e87
No Context <>
2021-04-22 04:37:46 -0500

    website

---
+++ precache-manifest.e4850720f38e9d779573ee04ca5169b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "682cf105fce37660cfb5ea2dd6fbd9f5",
+    "url": "/static/media/Yin_Yang_Cat_1024x1024.682cf105.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a04658966f439ba094ba",
+    "url": "/static/js/main.a0465896.chunk.js"
+  },
+  {
+    "revision": "0447f35bfaa2752017f4",
+    "url": "/static/js/1.0447f35b.chunk.js"
+  },
+  {
+    "revision": "a04658966f439ba094ba",
+    "url": "/static/css/main.d60089d4.chunk.css"
+  },
+  {
+    "revision": "0447f35bfaa2752017f4",
+    "url": "/static/css/1.947bfb05.chunk.css"
+  },
+  {
+    "revision": "42acec4357b81933923ff9317a63c5b7",
+    "url": "/index.html"
+  }
+];
